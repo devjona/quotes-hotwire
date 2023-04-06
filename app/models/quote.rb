385039e179e147a53ca81a9f5b1e3eb90c65c4ec
Jona Dev <7801456+devjona@users.noreply.github.com>
2023-04-06 17:14:15 -0500

    Adds TurboStream to the Index page, adds Redis functionality.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,13 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  after_create_commit do
+    broadcast_prepend_to "quotes",
+                         partial: "quotes/quote",
+                         locals: {
+                           quote: self
+                         },
+                         target: "quotes"
+  end
 end
