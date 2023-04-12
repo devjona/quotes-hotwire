469d909a60813a67025c68fae3b444586b5946e7
Jona Dev <7801456+devjona@users.noreply.github.com>
2023-04-12 07:49:27 -0500

    - Adds Navbar HTML, CSS, erb logic to display company name and sign in/out - Updates routes - Adds pages views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+  # get 'pages/home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'quotes#index'
+  # root 'quotes#index'
   resources :quotes
 end
