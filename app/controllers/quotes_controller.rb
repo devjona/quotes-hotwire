41cc0cfa062fbcc25382882cf5d618b144e8e314
Jona Dev <7801456+devjona@users.noreply.github.com>
2023-04-21 22:32:18 -0500

    Flash messages now work with Hotwire but I need to review all of this to make sure that I'm absorbing. Practice, repetition.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -20,7 +20,9 @@ class QuotesController < ApplicationController
         format.html do
           redirect_to quotes_path, notice: "Quote was successfully created."
         end
-        format.turbo_stream
+        format.turbo_stream do
+          flash.now[:notice] = "Quote was successfully created."
+        end
       end
     else
       render :new
@@ -32,9 +34,16 @@ class QuotesController < ApplicationController
 
   def update
     if @quote.update(quote_params)
-      redirect_to quotes_path, notice: "Quote was successfully updated."
+      respond_to do |format|
+        format.html do
+          redirect_to quotes_path, notice: "Quote was successfully updated."
+        end
+        format.turbo_stream do
+          flash.now[:notice] = "Quote was successfully updated."
+        end
+      end
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -45,7 +54,7 @@ class QuotesController < ApplicationController
       format.html do
         redirect_to quotes_url, notice: "Quote was successfully destroyed."
       end
-      format.turbo_stream
+      format.turbo_stream { flash.now[:notice] = "Quote was successfully destroyed."}
     end
   end
 
